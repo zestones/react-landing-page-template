2f1b71e6d3f75dc5faf096fe42a0737e1066acba
zestones <idrissbenguezzou@gmail.com>
2024-07-30 01:04:10 +0200

    feat: responsive community section

--- agrovia/src/App.jsx
+++ agrovia/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Header />
       <HeroSection />
       <Community />
-      <Unlock
+      {/* <Unlock
         icon={PhoneIcon}
         title="The unseen of spending three <br />years at Pixelgrade"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio."
@@ -31,7 +31,7 @@ function App() {
         description="Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida."
       />
       <Banner />
-      <Footer />
+      <Footer /> */}
     </>
   )
 }
