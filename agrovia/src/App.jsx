90d08cccc4fad838ad08fb03e91f5af9c603d941
zestones <idrissbenguezzou@gmail.com>
2024-07-30 01:33:33 +0200

    feat: responsive footer components

--- agrovia/src/App.jsx
+++ agrovia/src/App.jsx
@@ -30,8 +30,8 @@ function App() {
         title="How to design your site footer like <br />we did"
         description="Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida."
       />
-      {/* <Banner />
-      <Footer /> */}
+      <Banner />
+      <Footer />
     </>
   )
 }
--- agrovia/src/components/Footer/Footer.jsx
+++ agrovia/src/components/Footer/Footer.jsx
@@ -1,56 +1,59 @@
-
-import Logo from '../../assets/logo.png'
-
-import InstagramIcon from '../../assets/instagram-icon.png'
-import WebsiteIcon from '../../assets/website-icon.png'
-import YoutubeIcon from '../../assets/youtube-icon.png'
-import TwitterIcon from '../../assets/twitter-icon.svg'
+import Logo from '../../assets/logo.png';
+import InstagramIcon from '../../assets/instagram-icon.png';
+import WebsiteIcon from '../../assets/website-icon.png';
+import YoutubeIcon from '../../assets/youtube-icon.png';
+import TwitterIcon from '../../assets/twitter-icon.svg';
 
 function Footer() {
-
     return (
-        <footer className="bg-neutral-black text-white" id='footer'>
-            <div className="container mx-auto flex flex-row justify-center items-center px-4 py-10 w-full">
-                <div className="flex flex-col space-y-4 w-1/4">
-                    <img src={Logo} alt="Agrovia" className="w-32 h-auto" />
-                    <p>Copyright &copy; 2024 Agrovia. <br /> All rights reserved.</p>
-                    <div className="flex space-x-2 mt-8">
-                        <img src={InstagramIcon} alt="Instagram" className="w-8" />
-                        <img src={WebsiteIcon} alt="Website" className="w-8" />
-                        <img src={YoutubeIcon} alt="Youtube" className="w-8" />
-                        <img src={TwitterIcon} alt="Twitter" className="w-8" />
+        <footer className="bg-neutral-black text-white py-10" id='footer'>
+            <div className="container mx-auto px-4">
+                <div className="flex flex-col items-center md:flex-row md:justify-between">
+                    <div className="flex flex-col items-center md:items-start mb-8 md:mb-0 text-center md:text-left w-full md:w-1/3">
+                        <img src={Logo} alt="Agrovia" className="w-32 h-auto mb-4" />
+                        <p className="text-sm md:text-base mb-4">
+                            Copyright &copy; 2024 Agrovia. <br /> All rights reserved.
+                        </p>
+                        <div className="flex space-x-4 justify-center md:justify-start">
+                            <img src={InstagramIcon} alt="Instagram" className="w-8 h-8" />
+                            <img src={WebsiteIcon} alt="Website" className="w-8 h-8" />
+                            <img src={YoutubeIcon} alt="Youtube" className="w-8 h-8" />
+                            <img src={TwitterIcon} alt="Twitter" className="w-8 h-8" />
+                        </div>
                     </div>
-                </div>
-                <div className="flex space-x-4 w-3/4 justify-center">
-                    <div className="flex flex-col w-1/4 items-start">
-                        <h3 className="text-lg font-semibold">Company</h3>
-                        <ul className="mt-2 space-y-2">
-                            <li className="text-neutral-silver">Home</li>
-                            <li className="text-neutral-silver">About</li>
-                            <li className="text-neutral-silver">Services</li>
-                            <li className="text-neutral-silver">Contact</li>
-                        </ul>
-                    </div>
-                    <div className="flex flex-col w-1/4 items-start">
-                        <h3 className="text-lg font-semibold">Resources</h3>
-                        <ul className="mt-2 space-y-2">
-                            <li className="text-neutral-silver">Blog</li>
-                            <li className="text-neutral-silver">FAQ</li>
-                            <li className="text-neutral-silver">Support</li>
-                        </ul>
-                    </div>
-                    <div className="flex flex-col w-1/4 items-start">
-                        <h3 className="text-lg font-semibold">Legal</h3>
-                        <ul className="mt-2 space-y-2">
-                            <li className="text-neutral-silver">Terms</li>
-                            <li className="text-neutral-silver">Privacy</li>
-                        </ul>
-                    </div>
-                </div>
 
-            </div >
-        </footer >
-    )
+                    <div className="flex flex-col md:flex-row md:space-x-8 w-full md:w-2/3">
+                        <div className="flex flex-col items-center mb-8 md:mb-0 w-full md:w-1/4">
+                            <h3 className="text-lg font-semibold mb-2">Company</h3>
+                            <ul className="space-y-2">
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Home</li>
+                                <li className="text-neutral-silver hover:text-primary-primary transition">About</li>
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Services</li>
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Contact</li>
+                            </ul>
+                        </div>
+
+                        <div className="flex flex-col items-center mb-8 md:mb-0 w-full md:w-1/4">
+                            <h3 className="text-lg font-semibold mb-2">Resources</h3>
+                            <ul className="space-y-2">
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Blog</li>
+                                <li className="text-neutral-silver hover:text-primary-primary transition">FAQ</li>
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Support</li>
+                            </ul>
+                        </div>
+
+                        <div className="flex flex-col items-center w-full md:w-1/4">
+                            <h3 className="text-lg font-semibold mb-2">Legal</h3>
+                            <ul className="space-y-2">
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Terms</li>
+                                <li className="text-neutral-silver hover:text-primary-primary transition">Privacy</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
