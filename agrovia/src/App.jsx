2914e1667d7dddd35081056210ff819f52815a56
zestones <idrissbenguezzou@gmail.com>
2024-07-30 01:26:52 +0200

    feat: responsive achievement section

--- agrovia/src/App.jsx
+++ agrovia/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         title="The unseen of spending three <br />years at Pixelgrade"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio."
       />
-      {/* <Achievements /> */}
+      <Achievements />
       <Unlock
         icon={MobileIcon}
         title="How to design your site footer like <br />we did"
