c93f76deb2ba78ca51a762d8e4bbccd2e04112d4
zestones <idrissbenguezzou@gmail.com>
2024-07-29 17:56:28 +0200

    refactor: unlcok component modulable

--- agrovia/src/App.jsx
+++ agrovia/src/App.jsx
@@ -5,6 +5,11 @@ import Community from './components/Community/Community'
 import Unlock from './components/Unlock/Unlock'
 import Achievements from './components/Achievements/Achievements'
 
+// Assets
+import PhoneIcon from './assets/phone-icon.png';
+import MobileIcon from './assets/mobile-login-icon.png';
+
+
 function App() {
 
   return (
@@ -12,8 +17,17 @@ function App() {
       <Header />
       <HeroSection />
       <Community />
-      <Unlock />
+      <Unlock
+        icon={PhoneIcon}
+        title="The unseen of spending three <br />years at Pixelgrade"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio."
+      />
       <Achievements />
+      <Unlock
+        icon={MobileIcon}
+        title="How to design your site footer like <br />we did"
+        description="Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida."
+      />
     </>
   )
 }
--- agrovia/src/components/Unlock/Unlock.jsx
+++ agrovia/src/components/Unlock/Unlock.jsx
@@ -1,21 +1,27 @@
+import PropTypes from 'prop-types';
 
-import PhoneIcon from '../../assets/phone-icon.png';
 
-
-function Unlock() {
+function Unlock({ icon, title, description }) {
     return (
         <section className="unlock flex flex-col md:flex-row justify-center items-center py-10">
             <div className="flex justify-center items-center px-4 py-10">
-                <img src={PhoneIcon} alt="Phone" className="max-w-full h-auto" />
+                <img src={icon} alt="Phone" className="max-w-full h-auto" />
             </div>
             <div className="md:w-1/2 px-8 py-10">
                 <div className="text-center md:text-left">
-                    <h2 className="h2 mb-4 text-neutral-d_grey">The unseen of spending three <br />years at Pixelgrade</h2>
-                    <p className="text-lg text-neutral-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                    <h2 className="h2 mb-4 text-neutral-d_grey">{title.split('<br />')[0]} <br /> {title.split('<br />')[1]}</h2>
+                    <p className="text-lg text-neutral-grey">{description}</p>
                 </div>
+                <button className="btn btn-primary btn-normal mt-8">Learn More</button>
             </div>
         </section>
     )
+}
+
+Unlock.propTypes = {
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
 }
 
 export default Unlock;
