2605e5cf74a9e2f14467f5faf322907aaaa92f4d
zestones <idrissbenguezzou@gmail.com>
2024-07-30 17:03:19 +0200

    update: text content

--- agrovia/src/components/Achievements/Achievements.jsx
+++ agrovia/src/components/Achievements/Achievements.jsx
@@ -8,43 +8,43 @@ function Achievements() {
     return (
         <section className="achievements bg-neutral-silver" id='achievements'>
             <div className="container mx-auto px-4 py-10 flex flex-col items-center sm:flex-col md:flex-col lg:flex-row justify-between">
-                <div className="text-center">
+                <div className="text-center lg:text-left">
                     <h2 className="h2 text-neutral-d_grey text-xl sm:text-2xl md:text-3xl">
-                        Helping a local <br />
-                        <span className="text-primary-primary">business reinvent itself</span>
+                        Aidant les entreprises <br />
+                        <span className="text-primary-primary">à se réinventer dans l&apos;agroalimentaire</span>
                     </h2>
                     <p className="text-base sm:text-lg md:text-xl text-neutral-grey mt-4">
-                        We reached here with our hard work and dedication
+                        Nous sommes arrivés ici grâce à notre travail acharné et notre dévouement
                     </p>
                 </div>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6 h-max">
-                    <div className="flex items-center justify-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6 h-max mt-8 lg:mt-0">
+                    <div className="flex items-center justify-start">
                         <img src={MembersIcon} alt="Members Icon" className="w-12 h-w-12" />
                         <div className="text-center sm:text-left px-4">
-                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">2,245,341</h3>
-                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Members</p>
+                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">1,234</h3>
+                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Partenaires</p>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-start">
                         <img src={CommunityIcon} alt="Community Icon" className="w-12 h-w-12" />
                         <div className="text-center sm:text-left px-4">
-                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">46,328</h3>
-                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Community</p>
+                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">567</h3>
+                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Inscrits</p>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-start">
                         <img src={PaymentIcon} alt="Payment Icon" className="w-12 h-w-12" />
                         <div className="text-center sm:text-left px-4">
-                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">1,926,436</h3>
-                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Payments</p>
+                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">9,876</h3>
+                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Transactions</p>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-start">
                         <img src={TouchIcon} alt="Touch Icon" className="w-12 h-w-12" />
                         <div className="text-center sm:text-left px-4">
-                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">828,867</h3>
-                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Event Bookings</p>
+                            <h3 className="h3 text-neutral-d_grey text-lg sm:text-xl md:text-2xl">4,321</h3>
+                            <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Contrats</p>
                         </div>
                     </div>
                 </div>
--- agrovia/src/components/Community/Community.jsx
+++ agrovia/src/components/Community/Community.jsx
@@ -8,26 +8,26 @@ function Community() {
         <section className="community" id='community'>
             <div className="container mx-auto px-4 py-10">
                 <div className="text-center">
-                    <h2 className="h2 mb-4 text-neutral-d_grey text-2xl sm:text-3xl md:text-4xl">Manage your entire community <br />in a single system</h2>
-                    <p className="text-base sm:text-lg md:text-xl text-neutral-grey">Who is Nextcent suitable for?</p>
+                    <h2 className="h2 mb-4 text-neutral-d_grey text-2xl sm:text-3xl md:text-4xl">Gérez toute votre communauté <br />dans un seul système</h2>
+                    <p className="text-base sm:text-lg md:text-xl text-neutral-grey">À qui s&apos;adresse Agrovia ?</p>
                 </div>
             </div>
             <div className="container mx-auto px-4 py-10 flex flex-col items-center">
                 <div className="flex flex-col sm:flex-row sm:justify-between space-y-4 sm:space-y-0 sm:space-x-4 w-full items-center">
                     <div className="community-card flex flex-col items-center max-w-xs w-full shadow-sm p-6 sm:p-8">
-                        <img src={MemberIcon} alt="Community" className='w-16 h-auto' />
-                        <h3 className="h3 mt-4 mb-2 text-neutral-d_grey text-center text-lg sm:text-xl md:text-2xl">Membership <br />Organisations</h3>
-                        <p className="text-base sm:text-lg md:text-lg text-neutral-grey text-center">Our membership management software provides full automation of membership renewals and payments</p>
+                        <img src={MemberIcon} alt="Grossistes" className='w-16 h-auto' />
+                        <h3 className="h3 mt-4 mb-2 text-neutral-d_grey text-center text-lg sm:text-xl md:text-2xl">Grossistes en<br />Produits Céréaliers</h3>
+                        <p className="text-base sm:text-lg md:text-lg text-neutral-grey text-center">Notre plateforme facilite la recherche de sources d&apos;approvisionnement fiables et propose des outils de négociation efficaces.</p>
                     </div>
                     <div className="community-card flex flex-col items-center max-w-xs w-full shadow-sm p-6 sm:p-8">
-                        <img src={FacilityIcon} alt="Facility" className='w-16 h-auto' />
-                        <h3 className="h3 mt-4 mb-2 text-neutral-d_grey text-center text-lg sm:text-xl md:text-2xl">National <br />Associations</h3>
-                        <p className="text-base sm:text-lg md:text-lg text-neutral-grey text-center">Our membership management software provides full automation of membership renewals and payments</p>
+                        <img src={FacilityIcon} alt="Brokers" className='w-16 h-auto' />
+                        <h3 className="h3 mt-4 mb-2 text-neutral-d_grey text-center text-lg sm:text-xl md:text-2xl">Courtiers en<br />Produits Céréaliers</h3>
+                        <p className="text-base sm:text-lg md:text-lg text-neutral-grey text-center">Nous aidons les courtiers à identifier des opportunités d&apos;achat avantageuses et à trouver des acheteurs potentiels.</p>
                     </div>
                     <div className="community-card flex flex-col items-center max-w-xs w-full shadow-sm p-6 sm:p-8">
-                        <img src={GroupsIcon} alt="Groups" className='w-16 h-auto' />
-                        <h3 className="h3 mt-4 mb-2 text-neutral-d_grey text-center text-lg sm:text-xl md:text-2xl">Clubs And<br /> Groups</h3>
-                        <p className="text-base sm:text-lg md:text-lg text-neutral-grey text-center">Our membership management software provides full automation of membership renewals and payments</p>
+                        <img src={GroupsIcon} alt="Importateurs/Exportateurs" className='w-16 h-auto' />
+                        <h3 className="h3 mt-4 mb-2 text-neutral-d_grey text-center text-lg sm:text-xl md:text-2xl">Importateurs et<br />Exportateurs</h3>
+                        <p className="text-base sm:text-lg md:text-lg text-neutral-grey text-center">Notre plateforme simplifie la gestion des échanges internationaux et facilite la recherche de partenaires commerciaux.</p>
                     </div>
                 </div>
             </div>
